53fc786b783fc7012de80bb214490659ca3f1cd0
chhotuthakur <npsdahiya@gmail.com>
2022-08-08 15:56:10 +0530

    Initial commit

---
+++ app/src/main/java/com/bluewaters/testdataportal/activities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.bluewaters.testdataportal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bluewaters.testdataportal.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TODO: add image view
+
+   /* TextView textView;
+    ImageView imageView;
+    String URL ="https://nileshsinghdahiya.in/";*/
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
