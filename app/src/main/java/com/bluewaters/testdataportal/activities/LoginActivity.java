1531614b4333df08c44347ecb1bdb87c5bb8aa31
chhotuthakur <npsdahiya@gmail.com>
2022-08-13 18:10:50 +0530

    updated UI by faizan bhaiya

---
+++ app/src/main/java/com/bluewaters/testdataportal/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.bluewaters.testdataportal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bluewaters.testdataportal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/bluewaters/testdataportal/activities/MainActivity.java
+++ app/src/main/java/com/bluewaters/testdataportal/activities/MainActivity.java
@@ -1,20 +1,26 @@
 package com.bluewaters.testdataportal.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.bluewaters.testdataportal.R;
+import com.bluewaters.testdataportal.fragments.AccountFragment;
+import com.bluewaters.testdataportal.fragments.HomeFragment;
+import com.bluewaters.testdataportal.fragments.ListFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: add image view
+BottomNavigationView bottomNavigationView;
+Fragment fragment;
 
-   /* TextView textView;
-    ImageView imageView;
-    String URL ="https://nileshsinghdahiya.in/";*/
+
 
 
 
@@ -23,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = (BottomNavigationView) findViewById(R.id.btm_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item ) {
+                Fragment fragmentload = null;
+                switch (item.getItemId()){
+                    case R.id.btm_home:
+                        fragmentload= new HomeFragment();
+                        break;
+                    case R.id.btm_acc:
+                        fragmentload = new AccountFragment();
+                        break;
+                    case R.id.btm_list:
+                        fragmentload = new ListFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragmentload).commit();
+                return true;
+            }
+        });
+
 
 
     }
